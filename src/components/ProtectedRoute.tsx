9cc709eb063dd5183f797c29a702283e0bac6860
Rachit Patel <rachitp509@gmail.com>
2024-03-15 11:21:27 +0530

    updated readme

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { RootState } from "../features/store";
 
 export default function ProtectedRoute({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { token } = useSelector((state: any) => state.auth);
+  const { token } = useSelector((state: RootState) => state.auth);
   return token ? children : <Navigate to="/login" />;
 }
