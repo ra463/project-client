b7525e6415ce4390402c97bcb2773c4b578a3ae5
Rachit Patel <rachitp509@gmail.com>
2024-03-15 08:48:39 +0530

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Pages/Auth/Login";
-import Register from "./Pages/Auth/Register";
-import Home from "./Pages/Home/Home";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import { useState } from "react";
-import AddProduct from "./Pages/Home/AddProduct";
+import { lazy, useState } from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
+
+const Home = lazy(() => import("./Pages/Home/Home"));
+const Register = lazy(() => import("./Pages/Auth/Register"));
+const Login = lazy(() => import("./Pages/Auth/Login"));
+const AddProduct = lazy(() => import("./Pages/Home/AddProduct"));
+const Header = lazy(() => import("./components/Header"));
+const Sidebar = lazy(() => import("./components/Sidebar"));
 
 function App() {
   const { token } = useSelector((state: any) => state.auth);
