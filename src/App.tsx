48f7f41bb8df2a8d733f9a2858e847d1da0ac17a
Rachit Patel <rachitp509@gmail.com>
2024-03-15 08:53:50 +0530

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, useState } from "react";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { useState } from "react";
+import AddProduct from "./Pages/Home/AddProduct";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
-
-const Home = lazy(() => import("./Pages/Home/Home"));
-const Register = lazy(() => import("./Pages/Auth/Register"));
-const Login = lazy(() => import("./Pages/Auth/Login"));
-const AddProduct = lazy(() => import("./Pages/Home/AddProduct"));
-const Header = lazy(() => import("./components/Header"));
-const Sidebar = lazy(() => import("./components/Sidebar"));
+import Home from "./Pages/Home/Home";
+import { RootState } from "./features/store";
 
 function App() {
-  const { token } = useSelector((state: any) => state.auth);
+  const { token } = useSelector((state: RootState) => state.auth);
   const [show, setShow] = useState(false);
 
   return (
